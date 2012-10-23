32bf3126cd9d8bb1d824f9e96d55a8fd004d6e86
Kelly Becker <kellylsbkr@gmail.com>
2012-10-22 22:49:52 -0700

    querylist base complete

--- _bundle.php
+++ _bundle.php
@@ -20,10 +20,20 @@ class Bundle {
 
 	public function _on_router_route($path) {
 		include(e\site . '/tmp.html');
-		$result = $this->__callBundle()->newList("members.account");
+		$result = $this->__callBundle()
+			->newList("members.account")
+			->joins("RIGHT JOIN `cms.page` ON `cms.page`.`id` = `members.account`.`id`")
+			//->where('`members.account`.`id` = ?', 1);
+			->order('`members.account`.`id`', 'DESC')
+			->limit(5,5);
+
+		//dump($result);
+		//dump($result->buildQuery());
+		dump($result->all());
 		foreach($result as $r) {
 			var_dump($r);echo("<br /><Br />");
 		}
+
 		e\Complete();
 	}
 
--- library/querylist.php
+++ library/querylist.php
@@ -43,7 +43,21 @@ class QueryList extends QueryResult {
 	 */
 	public function __construct(Connection $connection, $table) {
 		$this->__connection = $connection;
-		$this->setTable($table);
+		$this->table($table);
+	}
+
+	/**
+	 * Set the table to query on
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function table($table = false) {
+		if(!$table) return $this->_table;
+
+		// Set the table
+		if(strpos($table, '`') === 0)
+			$this->_table = $table;
+		else $this->_table = "`$table`";
 	}
 
 	/**
@@ -51,7 +65,7 @@ class QueryList extends QueryResult {
 	 * @author Kelly Becker
 	 * @since Oct 22nd, 2012
 	 */
-	public function replaceSelect($field = '*') {
+	public function resetSelect($field = '*') {
 
 		// Empty out the select array
 		$this->_select = array();
@@ -65,7 +79,7 @@ class QueryList extends QueryResult {
 	 * @author Kelly Becker
 	 * @since Oct 22nd, 2012
 	 */
-	public function addSelect($field = false) {
+	public function select($field = false) {
 		if(!$field) return $this;
 
 		// If the passed field is an array
@@ -80,7 +94,7 @@ class QueryList extends QueryResult {
 		}
 
 		// If no ticks are on the field
-		elseif(strpos($field, '`') !== 0) $field = "`$field`";
+		if(strpos($field, '`') !== 0 && strpos($field, ' as ') === false) $field = "`$field`";
 
 		// If not an array make it one and merge and return
 		$this->_select = array_merge($this->_select, array($field));
@@ -88,14 +102,189 @@ class QueryList extends QueryResult {
 	}
 
 	/**
-	 * Set the table to query on
+	 * Reset table joins
 	 * @author Kelly Becker
 	 * @since Oct 22nd, 2012
 	 */
-	public function setTable($table) {
-		if(strpos($table, '`') === 0)
-			$this->_table = $table;
-		else $this->_table = "`$table`";
+	public function resetJoins() {
+		$this->_joins = array();
+		return $this;
+	}
+
+	/**
+	 * Add table joins
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 * @todo Convert join shortcuts into architect join shortcuts
+	 */
+	public function joins($join = false) {
+		if(!$join) return $this;
+
+		if(is_array($join)) {
+
+			foreach($join as $j) {
+
+				// Split the join string into three parts
+				list($table, $dir, $field) = explode(' ', $j);
+
+				// Tick off some variables
+				if(strpos($table, '`') !== 0) $table = "`$table`";
+				if(strpos($field, '`') !== 0) $field = "`$field`";
+
+				// Add table to the field
+				$field = $table.'.'.$field;
+
+				// Always assume ID for base table
+				$field1 = $this->_table.'.`id`';
+
+				// Determine join direction
+				if($dir === '<') $j = 'LEFT';
+				if($dir === '>') $j = 'RIGHT';
+
+				// Or assume center join
+				$j .= 'JOIN '.$table.' ';
+				$j .= 'ON '.$field1.' = '.$field;
+
+				// Add join to the list
+				$this->joins($j);
+			}
+
+			return $this;
+		}
+
+		// Append the join
+		$this->_joins[] = $join;
+		return $this;
+	}
+
+	/**
+	 * Reset where conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function resetWhere() {
+		$this->_conditions = array();
+		return $this;
+	}
+
+	/**
+	 * Add where conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function where() {
+		$args = func_get_args();
+
+		// Get the base query
+		$query = array_shift($args);
+
+		// With no query do nothing
+		if(empty($query)) return $this;
+
+		// Handle sprinting
+		if(!empty($args)) {
+			$query = str_replace('?', '\'%s\'', $query);
+			$query = vsprintf($query, $args);
+		}
+
+		$this->_conditions[] = '('.$query.')';
+		return $this;
+	}
+
+	/**
+	 * Reset group by conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function resetGroup() {
+		$this->_group = array();
+		return $this;
+	}
+
+	/**
+	 * Add group by conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function group($group = false) {
+		if(!$group) return $this;
+
+		if(is_array($group)) {
+
+			// Add each to the group by
+			foreach($group as $g)
+				$this->group($g);
+
+			return $this;
+		}
+
+		// If not ticked off then tick off
+		if(strpos($group, '`') !== 0) $group = "`$group`";
+
+		// Add to the group by
+		$this->_group[] = $group;
+
+		return $this;
+	}
+
+	/**
+	 * Reset order by conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function resetOrder() {
+		$this->_order = array();
+		return $this;
+	}
+
+	/**
+	 * Add order by conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function order($order = false, $dir = 'ASC') {
+		if(!$order) return $this;
+
+		if(is_array($order)) {
+
+			// Add each to the order by
+			foreach($order as $o => $d)
+				$this->order($o, $d);
+
+			return $this;
+		}
+
+		// If not ticked off then tick off
+		if(strpos($order, '`') !== 0) $order = "`$order`";
+
+		// Add to the order by
+		$this->_order[] = $order.' '.$dir;
+
+		return $this;
+	}
+
+	/**
+	 * Reset limit conditions
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function resetLimit() {
+		$this->_limit = array();
+		return $this;
+	}
+
+	/**
+	 * Add limit condition
+	 * @author Kelly Becker
+	 * @since Oct 22nd, 2012
+	 */
+	public function limit($offset = false, $limit = false) {
+		if($limit && !is_numeric($limit)) return $this;
+		if(!is_numeric($offset)) return $this;
+
+		// Add the limit
+		$this->_limit = func_get_args();
+		return $this;
 	}
 
 	/**
@@ -121,7 +310,8 @@ class QueryList extends QueryResult {
 		$query[] = implode(' ', $this->_joins);
 
 		// Add the conditions with and
-		$query[] = implode(' && ', $this->_conditions);
+		if(!empty($this->_conditions))
+			$query[] = 'WHERE '.implode(' && ', $this->_conditions);
 
 		// Add group by
 		if(!empty($this->_group))
